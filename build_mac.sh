069ad0c5f1f05b6c0f64631933a763567aea98ec
rockihack <rocki.hack@gmail.com>
2017-08-30 18:46:27 +0200

    Fix build on mac os with qt 5.9.1

--- build_mac.sh
+++ build_mac.sh
@@ -5,9 +5,9 @@
 # > brew install cmake qt5 libgcrypt
 
 if cd build; then
-    QT5_DIR="/usr/local/opt/qt5"
+    QT5_DIR="/usr/local/opt/qt"
 
-    export MACOSX_DEPLOYMENT_TARGET=10.7
+    export MACOSX_DEPLOYMENT_TARGET=10.10
     cmake .. -DCMAKE_PREFIX_PATH=$QT5_DIR -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release
     make -j4
 fi
